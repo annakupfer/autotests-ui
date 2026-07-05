5a7c449bbc4ba70c2050bc4882490738f113e5ac
annakupfer <a.klyueva@gmail.com>
2026-07-05 21:15:21 +0300

    Работа с авторизацией в Playwright

---
+++ playwright_courses.py
@@ -0,0 +1,63 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем поле Email
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле Username
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill("username")
+
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+
+    # Нажимаем на кнопку Registration
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # Запоминаем состояние браузера
+    context.storage_state(path='browser-state.json')
+
+    # Создаем новую сессию браузера
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state = 'browser-state.json')
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    # Проверяется наличие и текст заголовка “Courses”
+    courses_list_toolbar_title_text = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_list_toolbar_title_text).to_be_visible()
+    expect(courses_list_toolbar_title_text).to_have_text('Courses')
+
+    # Проверяется наличие и видимость иконки пустого блока
+    courses_list_empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(courses_list_empty_view_icon).to_be_visible()
+
+    # Проверяется наличие и текст блока “There is no results”
+    courses_list_empty_view_title_text = page.get_by_test_id(
+        'courses-list-empty-view-title-text')
+    expect(courses_list_empty_view_title_text).to_be_visible()
+    expect(courses_list_empty_view_title_text).to_have_text('There is no results')
+
+    # Проверяется наличие и текст описания блока:
+    # "Results from the load test pipeline will be displayed here"
+    courses_list_empty_view_description_text = page.get_by_test_id(
+        'courses-list-empty-view-description-text')
+    expect(courses_list_empty_view_description_text).to_be_visible()
+    expect(courses_list_empty_view_description_text).to_have_text(
+        'Results from the load test pipeline will be displayed here')
+
+    # Задержка для наглядности выполнения теста
+    page.wait_for_timeout(5000)
+
+
