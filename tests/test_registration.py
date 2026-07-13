6fb2e884571d6283606720e0a6be1f47b8deeb9f
annakupfer <a.klyueva@gmail.com>
2026-07-13 21:18:15 +0300

    Знакомство с PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,31 +1,20 @@
 
-from playwright.sync_api import expect, Page
 import pytest
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):
+def test_successful_registration(
+        registration_page: RegistrationPage,
+        dashboard_page: DashboardPage,
+) -> None:
 
-        # Переходим на страницу входа
-        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.fill_registration_form(
+                email="user.name@gmail.com",
+                username="username",
+                password="password")
+        registration_page.click_registration_button()
+        dashboard_page.check_visible_dashboard_title()
 
-        # Заполняем поле Email
-        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill("user.name@gmail.com")
-
-        # Заполняем поле Username
-        username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill("username")
-
-        # Заполняем поле пароль
-        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill("password")
-
-        # Нажимаем на кнопку Registration
-        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-        expect(dashboard_title).to_be_visible()
-
-        
