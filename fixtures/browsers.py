24a36f714570a2004b2095c101cb31c6b94a9963
annakupfer <a.klyueva@gmail.com>
2026-08-05 14:50:27 +0300

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,23 +1,14 @@
 import pytest
-import allure
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
-from allure_commons.types import AttachmentType
 
 from pages.authentification.registration_page import RegistrationPage
+from tools.playwright.pages.initialize_playwright_page import initialize_playwright_page
+
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True,snapshots=True,sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 @pytest.fixture(scope= "session")
 def initialize_browser_state(playwright: Playwright) -> None:
@@ -38,14 +29,4 @@ def initialize_browser_state(playwright: Playwright) -> None:
 
 @pytest.fixture
 def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
-
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state= 'browser-state.json')
