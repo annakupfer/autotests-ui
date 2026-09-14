39d1a3f921ba53de4dd3e8d847c2f5d05568370a
annakupfer <a.klyueva@gmail.com>
2026-09-14 10:40:36 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/button.py
+++ elements/button.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import expect
 import allure
 from tools.logger import get_logger  # Импортируем get_logger
+from ui_coverage_tool import ActionType
 from elements.base_element import BaseElement
 
 logger = get_logger("BUTTON")  # Инициализируем logger
@@ -16,9 +17,13 @@ class Button(BaseElement):
             logger.info(step)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/textarea.py
+++ elements/textarea.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Locator, expect
 import allure
 from elements.base_element import BaseElement
+from ui_coverage_tool import ActionType
 from tools.logger import get_logger
 
 logger = get_logger("TEXTAREA")
@@ -13,6 +14,9 @@ class Textarea(BaseElement):
         # Получаем локатор textarea
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f"({super().get_raw_locator(nth, **kwargs)})//textarea[1]"
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
@@ -20,9 +24,13 @@ class Textarea(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that  {self.type_of} "{self.name}" has value "{value}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
